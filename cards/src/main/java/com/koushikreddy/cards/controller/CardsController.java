47cac057c1cd4f01342b62a073f5dd2384446ffe
Koushik Reddy Gajjala <kgajjala@csuchico.edu>
2024-05-30 22:33:29 -0700

    feat: Add validation annotations to CardsDto fields

--- cards/src/main/java/com/koushikreddy/cards/controller/CardsController.java
+++ cards/src/main/java/com/koushikreddy/cards/controller/CardsController.java
@@ -11,11 +11,14 @@ import com.koushikreddy.cards.dto.CardsDto;
 import com.koushikreddy.cards.dto.ResponseDto;
 import com.koushikreddy.cards.service.ICardsService;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,6 +26,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController()
 @RequestMapping(path = "/api", produces = { MediaType.APPLICATION_JSON_VALUE })
 @AllArgsConstructor
+@Validated
 public class CardsController {
 
     private final ICardsService iCardsService;
@@ -33,14 +37,16 @@ public class CardsController {
     }
 
     @PostMapping("/create")
-    public ResponseEntity<ResponseDto> createCard(@RequestParam String mobileNumber) {
+    public ResponseEntity<ResponseDto> createCard(
+            @Valid @RequestParam @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits") String mobileNumber) {
         iCardsService.createCard(mobileNumber);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(new ResponseDto(CardsConstants.STATUS_201, CardsConstants.MESSAGE_201));
     }
 
     @GetMapping("/fetch")
-    public ResponseEntity<CardsDto> fetchCard(@RequestParam String mobileNumber) {
+    public ResponseEntity<CardsDto> fetchCard(
+            @Valid @RequestParam @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits") String mobileNumber) {
 
         CardsDto cardsDto = iCardsService.fetchCard(mobileNumber);
 
@@ -48,7 +54,7 @@ public class CardsController {
     }
 
     @PutMapping("/update")
-    public ResponseEntity<ResponseDto> updateCard(@RequestBody CardsDto cardsDto) {
+    public ResponseEntity<ResponseDto> updateCard(@Valid @RequestBody CardsDto cardsDto) {
 
         boolean isUpdated = iCardsService.updateCard(cardsDto);
 
@@ -62,7 +68,8 @@ public class CardsController {
     }
 
     @DeleteMapping("/delete")
-    public ResponseEntity<ResponseDto> deleteCard(@RequestParam String mobileNumber) {
+    public ResponseEntity<ResponseDto> deleteCard(
+            @Valid @RequestParam @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits") String mobileNumber) {
 
         boolean isUpdated = iCardsService.deleteCard(mobileNumber);
 
--- cards/src/main/java/com/koushikreddy/cards/dto/CardsDto.java
+++ cards/src/main/java/com/koushikreddy/cards/dto/CardsDto.java
@@ -1,14 +1,31 @@
 package com.koushikreddy.cards.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Data;
 
 @Data
 public class CardsDto {
 
+    @NotEmpty(message = "Mobile Number Cannot be empty")
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile Number must be 10 digits")
     private String mobileNumber;
+
+    @NotEmpty(message = "Card Number Cannot be empty")
+    @Pattern(regexp = "(^$|[0-9]{12})", message = "Card Number Must be 12 digits")
     private String cardNumber;
+
+    @NotEmpty(message = "Card Type cannot be null or empty")
     private String cardType;
+
+    @Positive(message = "Total card limit should be greater than 0")
     private int totalLimit;
+
+    @PositiveOrZero(message = "Total amount should be equal or greater tham zero")
     private int amountUsed;
+
+    @PositiveOrZero(message = "Available amaount should be equal or greater than zero")
     private int availableAmount;
 }
