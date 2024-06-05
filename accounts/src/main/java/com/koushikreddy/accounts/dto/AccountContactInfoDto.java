345f146a1c3c22803be5be012e8e7875e512451d
Koushik Reddy Gajjala <kgajjala@csuchico.edu>
2024-06-04 22:10:34 -0700

    refactor: Update application configuration for different environments

--- accounts/src/main/java/com/koushikreddy/accounts/dto/AccountContactInfoDto.java
+++ accounts/src/main/java/com/koushikreddy/accounts/dto/AccountContactInfoDto.java
@@ -5,7 +5,15 @@ import java.util.Map;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 @ConfigurationProperties(prefix = "accounts")
-public record AccountContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
-    
+public class AccountContactInfoDto {
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
+
 }
--- cards/src/main/java/com/koushikreddy/cards/dto/CardContactInfoDto.java
+++ cards/src/main/java/com/koushikreddy/cards/dto/CardContactInfoDto.java
@@ -5,8 +5,14 @@ import java.util.Map;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 @ConfigurationProperties(prefix = "cards")
-public record CardContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+public class CardContactInfoDto {
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 }
-
-
